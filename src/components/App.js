268298956cac1f57dc80e16fecf78a60be258f41
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-07-17 12:09:06 -0400

    [NEW] Components for showing albums

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AlbumContainer from './AlbumContainer'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AlbumContainer
     </div>
   );
 }
